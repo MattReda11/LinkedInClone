5ae91bd63e832b46ea57ea247e0494c69384b62a
SeenaSK <104446906+SeenaSK@users.noreply.github.com>
2023-02-01 12:37:02 -0500

    Connection Entity created

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -10,26 +10,26 @@ using Microsoft.EntityFrameworkCore;
 namespace LinkedInClone.Data
 {
     //main DB context that will contain Identity context
-    public class AppDbContext: IdentityDbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
-         public DbSet<IdentityUser> AspNetUsers { get; set; }
-         public DbSet<Message> Messages { get; set; }
+        public DbSet<IdentityUser> AspNetUsers { get; set; }
+        public DbSet<Message> Messages { get; set; }
         // public DbSet<Comment> Comments { get; set; }
-        // public DbSet<Connection> Connections { get; set; }
+        public DbSet<Connection> Connections { get; set; }
 
         //  UserManager<IdentityUser> _userManager;
         // RoleManager<IdentityRole> _roleManager;
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-           
+
         }
         protected override async void OnModelCreating(ModelBuilder modelBuilder)
         {
             //relationships should be specified here
-            
+
             base.OnModelCreating(modelBuilder);
         }
     }
