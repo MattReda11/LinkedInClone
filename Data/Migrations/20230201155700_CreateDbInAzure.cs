f235da923de08d6156f6cd2ec1360f08d8278b63
MattReda11 <103293959+MattReda11@users.noreply.github.com>
2023-02-01 11:00:43 -0500

    database connected to Azure

---
+++ Data/Migrations/20230201155700_CreateDbInAzure.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LinkedInClone.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDbInAzure : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using LinkedInClone.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("LinkedInCloneIdentityDbContextConnection") ?? throw new InvalidOperationException("Connection string 'LinkedInCloneIdentityDbContextConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("AppDbContext") ?? throw new InvalidOperationException("Connection string 'AppDbContext' not found.");
 
 builder.Services.AddDbContext<LinkedInCloneIdentityDbContext>(options => options.UseSqlServer(connectionString));
 
