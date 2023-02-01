76b1b2191e0d65e5109c7fad616b23b84f2e6f2c
Kevin <92401368+kevindarbydev@users.noreply.github.com>
2023-02-01 12:28:14 -0500

    deleted old context and all references

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using LinkedInClone.Areas.Identity.Data;
+
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using LinkedInClone.Data;
 
