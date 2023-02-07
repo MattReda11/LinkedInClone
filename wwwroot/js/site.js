1f098d1a089de76f2664442332d4d01d5eac3e4b
ChristopherPerrault <christopherperrault@hotmail.com>
2023-02-06 23:40:03 -0500

    comment button

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,35 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//  on clicking Comment button from Index, shows or hides the comment input while toggling the button text, tooltip text, icon, and color depending on hidden state. In "Cancel" state a click will also clear the input field.
 
-// Write your JavaScript code.
+async function ShowCommentBox() {
+  let commentboxDiv = document.getElementById("commentbox");
+  let btnLink = document.getElementById("comment-link");
+  let commentBtn = document.getElementById("comment-btn");
+  let commentInput = document.getElementById("comment-input");
+  let btnIcon = document.getElementById("comment-btn-icon");
+
+  if (commentboxDiv.hasAttribute("hidden")) {
+    commentboxDiv.removeAttribute("hidden");
+    btnLink.innerHTML = "Cancel";
+
+    commentBtn.setAttribute("data-tooltip", "Cancel your comment");
+    commentBtn.classList.remove("btn-transparent");
+    commentBtn.classList.add("btn-warning");
+    commentBtn.addEventListener("click", () => {
+      commentInput.value = "";
+    });
+
+    btnIcon.classList.remove("fa-regular", "fa-comment-dots", "fa-xl");
+    btnIcon.classList.add("fa-solid", "fa-xmark");
+  } else {
+    commentboxDiv.hidden = true;
+
+    btnLink.innerHTML = "Comment";
+
+    commentBtn.setAttribute("data-tooltip", "Leave a comment");
+    commentBtn.classList.remove("btn-warning");
+    commentBtn.classList.add("btn-transparent");
+
+    btnIcon.classList.remove("fa-solid", "fa-xmark");
+    btnIcon.classList.add("fa-regular", "fa-comment-dots", "fa-xl");
+  }
+}
